106c5ff51adae4f30260da7ee3decf4d4e9b916e
Unknown <clayjlindstrom@gmail.com>
2018-01-18 19:46:29 -0700

    added a unity game file

---
+++ CodingClassGame/Assets/Scripts/CubeMovingScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovingScript : MonoBehaviour {
+	private Vector3 mover;
+	private Rigidbody rb3d;
+
+	// Use this for initialization
+	void Start () {
+		rb3d = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mover = rb3d.velocity;
+		if(Input.GetKey(KeyCode.W)){
+			mover.z = 20;
+		}
+		if(Input.GetKey(KeyCode.A)){
+			mover.x = -20;
+		}
+		if(Input.GetKey(KeyCode.S)){
+			mover.z = -20;
+		}
+		if(Input.GetKey(KeyCode.D)){
+			mover.x = 20; 
+		}
+		if(Input.GetKey(KeyCode.Space)){
+			mover.y = 20;
+		}
+		rb3d.velocity = mover;
+	}
+}
