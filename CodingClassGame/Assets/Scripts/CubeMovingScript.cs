8568cd3fb29aba2e5c19c4f92dfd0045810617a6
Unknown <clayjlindstrom@gmail.com>
2018-01-25 19:25:54 -0700

    Progress:
    Edited the controls of the cube to be able to move it more efficiently.

--- CodingClassGame/Assets/Scripts/CubeMovingScript.cs
+++ CodingClassGame/Assets/Scripts/CubeMovingScript.cs
@@ -3,32 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeMovingScript : MonoBehaviour {
-	private Vector3 mover;
-	private Rigidbody rb3d;
+
+	//public float transx, transy, transz, rotx, roty, rotz ;
+
+	public Vector3 mover;
+	public Quaternion rotate;
+
+	public float moveSpeed, turnSpeed, jumpHeight;
 
 	// Use this for initialization
 	void Start () {
-		rb3d = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		mover = rb3d.velocity;
-		if(Input.GetKey(KeyCode.W)){
-			mover.z = 20;
-		}
-		if(Input.GetKey(KeyCode.A)){
-			mover.x = -20;
-		}
-		if(Input.GetKey(KeyCode.S)){
-			mover.z = -20;
+		var j = Input.GetAxis("Jump") * Time.deltaTime * jumpHeight;
+		var y = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+		transform.Translate(0,0,z);
+		transform.Translate(0,j,0);
+		transform.Rotate(0,y,0);
+
+		/*if(Input.GetKey(KeyCode.W)){
+			transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 		}
 		if(Input.GetKey(KeyCode.D)){
-			mover.x = 20; 
+			transform.Translate(Vector3.right * moveSpeed *  Time.deltaTime);
 		}
-		if(Input.GetKey(KeyCode.Space)){
-			mover.y = 20;
+		if(Input.GetKey(KeyCode.S)){
+			transform.Translate(Vector3.back * moveSpeed *  Time.deltaTime);
 		}
-		rb3d.velocity = mover;
+		if(Input.GetKey(KeyCode.A)){
+			transform.Translate(Vector3.left * moveSpeed *  Time.deltaTime);
+		}*/
 	}
 }
