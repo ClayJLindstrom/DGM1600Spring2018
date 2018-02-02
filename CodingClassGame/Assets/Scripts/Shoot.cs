e449c6f74696782e1a7fc8cbb0b0aa63ea694e8f
Unknown <clayjlindstrom@gmail.com>
2018-02-01 19:56:25 -0700

    Added some targets!

---
+++ CodingClassGame/Assets/Scripts/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+	public Rigidbody projectile;
+	public Transform shootPoint;
+	public int shootSpeed;
+	public bool fire;
+
+	// Use this for initialization
+	void Start () {
+		fire = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			fire = !fire;
+		}
+
+		if(fire){
+			Rigidbody clone;
+			clone = (Rigidbody)Instantiate(projectile, shootPoint.position, projectile.rotation);
+			clone.velocity = shootPoint.TransformDirection (Vector3.up * shootSpeed);
+		}
+	}
+}
