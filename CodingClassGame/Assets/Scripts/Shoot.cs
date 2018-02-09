69e5ac2bfcec5628d87f9e43e60e3e32440d7038
Unknown <clayjlindstrom@gmail.com>
2018-02-08 19:46:32 -0700

    made some friends for the tank.

--- CodingClassGame/Assets/Scripts/Shoot.cs
+++ CodingClassGame/Assets/Scripts/Shoot.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Shoot : MonoBehaviour {
 	public Rigidbody projectile;
 	public Transform shootPoint;
-	public int shootSpeed;
-	public bool fire;
+	public int bulletSpeed;
+	public bool fire, fireSpeed;
+	public float bulletCooldown, timer;
 
 	// Use this for initialization
 	void Start () {
 		fire = false;
+		bulletCooldown = 0.04f;
+		timer = 0f;
 	}
 	
 	// Update is called once per frame
@@ -18,11 +21,14 @@ public class Shoot : MonoBehaviour {
 		if(Input.GetButtonDown("Fire1")){
 			fire = !fire;
 		}
+		
 
-		if(fire){
+		if(fire && timer <= 0){
 			Rigidbody clone;
 			clone = (Rigidbody)Instantiate(projectile, shootPoint.position, projectile.rotation);
-			clone.velocity = shootPoint.TransformDirection (Vector3.up * shootSpeed);
+			clone.velocity = shootPoint.TransformDirection (Vector3.up * bulletSpeed);
+			timer = bulletCooldown;
 		}
+		else if(fire){timer -= Time.deltaTime;}
 	}
 }
---
+++ CodingClassGame/Assets/Scripts/VictimScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictimScript : MonoBehaviour {
+	public float moveSpeed;
+	public Transform target, chickenPen;
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay(Collider other){
+		if(other.gameObject.tag == "Player"){
+			Debug.Log("Target Acquired!");
+			target = other.gameObject.transform;
+			transform.LookAt(target);
+			transform.Translate(Vector3.back* moveSpeed*Time.deltaTime);
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.tag == "Respawn"){
+			transform.position = chickenPen.position;
+			transform.rotation = chickenPen.rotation;
+		}
+	}
+}
